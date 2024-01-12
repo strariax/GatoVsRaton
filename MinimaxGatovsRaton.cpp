f54821c89fe4feaf4d17dd2bda106360405ef875
strariax <clarajazmincespedes@gmail.com>
2024-01-12 14:36:17 -0300

    Correción de errore, aplicación del código
    - Se encontró que el juego fallaba para profundidades grandes de
    análisis debido a que para determinadas posiciones el ratón podía comer
    al gato.
    - Se creó una estructura llamada posición, para ayudar a encontrar a los
    jugadores dentro del tablero.
    - Se creó una función para encontrarJugador, que devuelva la
    estructura con la posición de cada jugador, esto para reaprovechar
    mejor el código, ya que buscar a los jugadores es algo que se realiza
    con frecuencia.
    - Se creó la función comprobarMovimiento, la cual ahorra la necesidad
    realizar un movimiento solamente para comprobar si este es posible.
    - Se eliminó sonMatricesIguales, en consecuencia al punto anterior.
    - Se eliminaron las funciones moverArriba, moverDerecha, moverIzquierda,
    moverAbajo y se creó una única función mover, que se encarga de todos
    los movimientos posibles.
    - Se actualizó minimax para que funcione de acuerdo a los cambios
    anteriores.
    - Se actualizaron las funciones jugarGato y jugarRaton, para que
    funcionen de acuerdo a los cambios anteriores.

--- MinimaxGatovsRaton.cpp
+++ MinimaxGatovsRaton.cpp
@@ -5,41 +5,53 @@
 
 using namespace std;
 
-const char GATO = 'G', RATON = 'R'; // Jugadores
+const char GATO = 'G', RATON = 'R', VACIO = ' '; // Jugadores
 const int FILAS = 10, COLUMNAS = 10; // Tamaño de nuestro tablero
 int fila_raton = 9, col_raton = 0; //Casilla de partida del ratón, dentro del tablero
 int fila_gato = 0, col_gato = 9; // Casilla de partida del gato, dentro del tablero
-int duracionDelJuego=20; // Cantidad de turnos que va durar cada juego
+int duracionDelJuego=30; // Cantidad de turnos que va durar cada juego
+
+struct posicion{
+    int fila;
+    int columna;
+    char jugador;
+    bool encontrado;
+
+    posicion(char j) : fila(-1), columna(-1), jugador(j), encontrado(false) {}
+    posicion(int f, int c, char j) : fila(f), columna(c), jugador(j), encontrado(true) {}
+};
+
 void inicializarTablero(vector<vector<char>>& matriz);
-void imprimirTablero(const vector<vector<char>>& matriz);
-vector<vector<char>> moverArriba(vector<vector<char>> matriz, char jugador);
-vector<vector<char>> moverAbajo(vector<vector<char>> matriz, char jugador);
-vector<vector<char>> moverDerecha(vector<vector<char>> matriz, char jugador);
-vector<vector<char>> moverIzquierda(vector<vector<char>> matriz, char jugador);
+void imprimirTablero(const vector<vector<char>>& matriz); // Const para que la matriz sea solo lectura
+bool comprobarMovimiento(const vector<vector<char>> matriz, char jugador, char movimiento);
+posicion encontrarJugador(const vector<vector<char>> matriz, char jugador);
 vector<vector<char>> jugarGato(vector<vector<char>> matriz, int profundidad, int turnosRestantes);
 vector<vector<char>> jugarRaton(vector<vector<char>> matriz, int profundidad, int turnosRestantes);
+vector<vector<char>> mover(vector<vector<char>> matriz, char jugador, char movimiento);
 int distanciaManhattan(vector<vector<char>> matriz);
 int minimax(vector<vector<char>> matriz, int profundidad, bool esTurnoRaton, int turnosRestantes);
 bool juegoTerminado(const vector<vector<char>> matriz, int turnosRestantes);
-bool sonMatricesIguales(const vector<vector<char>>& matriz1, const vector<vector<char>>& matriz2);
-
 
 int main(){
 
     vector<vector<char>> tablero(FILAS, vector<char>(COLUMNAS, ' '));
-    vector<vector<char>> nuevoTablero;
 
     inicializarTablero(tablero);
     imprimirTablero(tablero);
 
     while (!juegoTerminado(tablero, duracionDelJuego))
     {
-       tablero = jugarRaton(tablero, 3, duracionDelJuego);
-       tablero = jugarGato(tablero, 4, duracionDelJuego);
-       duracionDelJuego--;
+       tablero = jugarRaton(tablero, 2, duracionDelJuego);
+       tablero = jugarGato(tablero, 2, duracionDelJuego);
        imprimirTablero(tablero);
+       duracionDelJuego--;
     }
-    
+    if (distanciaManhattan(tablero) == 0){
+        cout<<"EL GATO ES EL GANADOR"<<endl;
+    }
+    else{
+        cout<<"EL RATON ES EL GANADOR"<<endl;
+    }
     return 0;
 }
 
@@ -79,97 +91,134 @@ void imprimirTablero(const vector<vector<char>>& matriz) {
     }
 }
 
-vector<vector<char>> moverArriba(vector<vector<char>> matriz, char jugador){
-    int fila_jugador, col_jugador;
-    for (int fila=0; fila<FILAS; fila++){
-        for (int col=0; col<COLUMNAS; col++){
+posicion encontrarJugador(const vector<vector<char>> matriz, char jugador){
+    for(int fila=0;fila<FILAS;fila++){
+        for(int col=0;col<COLUMNAS;col++){
             if(matriz[fila][col] == jugador){
-                fila_jugador = fila;
-                col_jugador = col;
+                return posicion(fila, col, jugador);
             }
         }
     }
-    if (fila_jugador-1 >= 0 && matriz[fila_jugador-1][col_jugador] != 'O'){
-        matriz[fila_jugador][col_jugador] = ' ';
-        fila_jugador--;
-        matriz[fila_jugador][col_jugador] = jugador;
+    // cout<<"No encuentro a: "<<jugador<<endl;
+    return posicion(jugador);
+}
+
+bool comprobarMovimiento(const vector<vector<char>> matriz, char jugador, char movimiento){
+    posicion posJugador = encontrarJugador(matriz, jugador);
+
+    if (posJugador.encontrado){
+        switch (movimiento)
+        {
+        case 'A': // Arriba
+            
+            if (posJugador.fila - 1 >=0 && matriz[posJugador.fila - 1][posJugador.columna] != 'O' && matriz[posJugador.fila - 1][posJugador.columna] != 'G'){
+                return true;
+            }
+            else{
+                return false;
+            }
+            break;
+        
+        case 'D': // Derecha
+            
+            if (posJugador.columna + 1 < COLUMNAS && matriz[posJugador.fila][posJugador.columna + 1] != 'O' && matriz[posJugador.fila][posJugador.columna + 1] != 'G'){
+                return true;
+            }
+            else{
+                return false;
+            }
+            break;
+        
+        case 'B': // Abajo
+            
+            if (posJugador.fila + 1 < FILAS && matriz[posJugador.fila + 1][posJugador.columna] != 'O' && matriz[posJugador.fila + 1][posJugador.columna] != 'G'){
+                return true;
+            }
+            else{
+                return false;
+            }
+            break;
+        
+        case 'I': // Izquierda
+            
+            if (posJugador.columna - 1 >=0 && matriz[posJugador.fila][posJugador.columna - 1] != 'O' && matriz[posJugador.fila][posJugador.columna - 1] != 'G'){
+                return true;
+            }
+            else{
+                return false;
+            }
+            break;
+        
+        default:
+            break;
+        }
+    }
+    return false;
+}
+
+vector<vector<char>> mover(vector<vector<char>> matriz, char jugador, char movimiento){
+    posicion posJugador = encontrarJugador(matriz, jugador);
+
+    if (posJugador.encontrado){
+        switch (movimiento)
+        {
+        case 'A': // Arriba
+            
+            if(comprobarMovimiento(matriz, jugador, movimiento)){
+
+                matriz[posJugador.fila][posJugador.columna] = VACIO;
+                matriz[posJugador.fila - 1][posJugador.columna] = jugador;
+
+            }
+            break;
+        
+        case 'D': // Derecha
+            
+            if(comprobarMovimiento(matriz, jugador, movimiento)){
+
+                matriz[posJugador.fila][posJugador.columna] = VACIO;
+                matriz[posJugador.fila][posJugador.columna + 1] = jugador;
+
+            }
+            break;
+        
+        case 'B': // Abajo
+            
+            if(comprobarMovimiento(matriz, jugador, movimiento)){
+
+                matriz[posJugador.fila][posJugador.columna] = VACIO;
+                matriz[posJugador.fila + 1][posJugador.columna] = jugador;
+
+            } 
+            break;
+        
+        case 'I': // Izquierda
+            
+            if(comprobarMovimiento(matriz, jugador, movimiento)){
+
+                matriz[posJugador.fila][posJugador.columna] = VACIO;
+                matriz[posJugador.fila][posJugador.columna - 1] = jugador;
+
+            }
+            break;
+        
+        default:
+            break;
+        }
     }
     return matriz;
 }
 
-vector<vector<char>> moverAbajo(vector<vector<char>> matriz, char jugador){
-    int fila_jugador, col_jugador;
-    for (int fila=0; fila<FILAS; fila++){
-        for (int col=0; col<COLUMNAS; col++){
-            if(matriz[fila][col] == jugador){
-                fila_jugador = fila;
-                col_jugador = col;
-            }
-        }
-    }
-    if (fila_jugador + 1 < FILAS && matriz[fila_jugador+1][col_jugador] != 'O'){
-        matriz[fila_jugador][col_jugador] = ' ';
-        fila_jugador++;
-        matriz[fila_jugador][col_jugador] = jugador;
-    }
-    return matriz;
-}
-
-vector<vector<char>> moverDerecha(vector<vector<char>> matriz, char jugador){
-    int fila_jugador, col_jugador;
-    for (int fila=0; fila<FILAS; fila++){
-        for (int col=0; col<COLUMNAS; col++){
-            if(matriz[fila][col] == jugador){
-                fila_jugador = fila;
-                col_jugador = col;
-            }
-        }
-    }
-    if (col_jugador + 1 < COLUMNAS && matriz[fila_jugador][col_jugador+1] != 'O'){
-        matriz[fila_jugador][col_jugador] = ' ';
-        col_jugador++;
-        matriz[fila_jugador][col_jugador] = jugador;
-    }
-    return matriz;
-}
-
-vector<vector<char>> moverIzquierda(vector<vector<char>> matriz, char jugador){
-    int fila_jugador, col_jugador;
-    for (int fila=0; fila<FILAS; fila++){
-        for (int col=0; col<COLUMNAS; col++){
-            if(matriz[fila][col] == jugador){
-                fila_jugador = fila;
-                col_jugador = col;
-            }
-        }
-    }
-    if (col_jugador - 1 >= 0 && matriz[fila_jugador][col_jugador-1] != 'O'){
-        matriz[fila_jugador][col_jugador] = ' ';
-        col_jugador--;
-        matriz[fila_jugador][col_jugador] = jugador;
-    }
-    return matriz;
-}
 
 int distanciaManhattan(vector<vector<char>> matriz) {
-    int fr = -1, cr = -1, fg, cg;
-    for (int fila=0; fila<FILAS; fila++){
-        for (int col=0; col<COLUMNAS; col++){
-            if(matriz[fila][col] == GATO){
-                fg = fila;
-                cg = col;
-            }else if(matriz[fila][col] == RATON){
-                fr = fila;
-                cr = col;
-            }
-        }
+
+    posicion gato = encontrarJugador(matriz, GATO);
+    posicion raton = encontrarJugador(matriz, RATON);
+
+    if(gato.encontrado && raton.encontrado){
+        return abs(gato.fila - raton.fila) + abs(gato.columna - raton.columna);
     }
-    if (fr == -1){
-        return 0;
-    }
-    else{
-        return abs(fg - fr) + abs(cg - cr);
-    }
+    return 0;
 }
 
 bool juegoTerminado(const vector<vector<char>> matriz, int turnosRestantes){
@@ -179,17 +228,6 @@ bool juegoTerminado(const vector<vector<char>> matriz, int turnosRestantes){
     return false;
 }
 
-bool sonMatricesIguales(const vector<vector<char>>& matriz1, const vector<vector<char>>& matriz2) {
-    for (int i = 0; i < FILAS; ++i) {
-        for (int j = 0; j < COLUMNAS; ++j) {
-            if (matriz1[i][j] != matriz2[i][j]) {
-                return false; // Las matrices son diferentes
-            }
-        }
-    }
-    return true; // Las matrices son iguales
-}
-
 int minimax(vector<vector<char>> matriz, int profundidad, bool esTurnoRaton, int turnosRestantes){
     if(profundidad == 0 || juegoTerminado(matriz, turnosRestantes)){
         return distanciaManhattan(matriz);
@@ -197,50 +235,26 @@ int minimax(vector<vector<char>> matriz, int profundidad, bool esTurnoRaton, int
     // Si es el turno del ratón debemos maximizar la evaluación (distancia Manhattan)
     if (esTurnoRaton){
         int maxEval = -1;
-        if(!sonMatricesIguales(matriz, moverArriba(matriz, RATON))){
-            matriz = moverArriba(matriz, RATON);
-            int eval = minimax(matriz, profundidad - 1, false, turnosRestantes);
-            matriz = moverAbajo(matriz, RATON);
-            maxEval = max(maxEval, eval);
-        } if(!sonMatricesIguales(matriz, moverDerecha(matriz, RATON))){
-            matriz = moverDerecha(matriz, RATON);
-            int eval = minimax(matriz, profundidad - 1, false, turnosRestantes);
-            matriz = moverIzquierda(matriz, RATON);
-            maxEval = max(maxEval, eval);
-        } if(!sonMatricesIguales(matriz, moverIzquierda(matriz, RATON))){
-            matriz = moverIzquierda(matriz, RATON);
-            int eval = minimax(matriz, profundidad - 1, false, turnosRestantes);
-            matriz = moverDerecha(matriz, RATON);
-            maxEval = max(maxEval, eval);
-        } if(!sonMatricesIguales(matriz, moverAbajo(matriz, RATON))){
-            matriz = moverAbajo(matriz, RATON);
-            int eval = minimax(matriz, profundidad - 1, false, turnosRestantes);
-            matriz = moverArriba(matriz, RATON);
-            maxEval = max(maxEval, eval);
+        vector<char> direcciones ={'A', 'D', 'B', 'I'};
+        for(int d=0;d<4;d++){
+            vector<vector<char>> tableroAux;
+            if(comprobarMovimiento(matriz, RATON, direcciones[d])){
+                tableroAux = mover(matriz, RATON, direcciones[d]);
+                int eval = minimax(tableroAux, profundidad - 1, false, turnosRestantes);
+                maxEval = max(maxEval, eval);
+            }
         }
         return maxEval;
     }else{
         int minEval = FILAS + COLUMNAS;
-        if(!sonMatricesIguales(matriz, moverArriba(matriz, GATO))){
-            matriz = moverArriba(matriz, GATO);
-            int eval = minimax(matriz, profundidad - 1, true, turnosRestantes - 1);
-            matriz = moverAbajo(matriz, GATO);
-            minEval = min(minEval, eval);
-        } if(!sonMatricesIguales(matriz, moverDerecha(matriz, GATO))){
-            matriz = moverDerecha(matriz, GATO);
-            int eval = minimax(matriz, profundidad - 1, true, turnosRestantes - 1);
-            matriz = moverIzquierda(matriz, GATO);
-            minEval = min(minEval, eval);
-        } if(!sonMatricesIguales(matriz, moverIzquierda(matriz, GATO))){
-            matriz = moverIzquierda(matriz, GATO);
-            int eval = minimax(matriz, profundidad - 1, true, turnosRestantes - 1);
-            matriz = moverDerecha(matriz, GATO);
-            minEval = min(minEval, eval);
-        } if(!sonMatricesIguales(matriz, moverAbajo(matriz, GATO))){
-            matriz = moverAbajo(matriz, GATO);
-            int eval = minimax(matriz, profundidad - 1, true, turnosRestantes - 1);
-            matriz = moverArriba(matriz, GATO);
-            minEval = min(minEval, eval);
+        vector<char> direcciones ={'A', 'D', 'B', 'I'};
+        for(int d=0;d<4;d++){
+            vector<vector<char>> tableroAux;
+            if(comprobarMovimiento(matriz, GATO, direcciones[d])){
+                tableroAux = mover(matriz, GATO, direcciones[d]);
+                int eval = minimax(tableroAux, profundidad - 1, true, turnosRestantes);
+                minEval = min(minEval, eval);
+            }
         }
         return minEval;
     }
@@ -249,49 +263,29 @@ int minimax(vector<vector<char>> matriz, int profundidad, bool esTurnoRaton, int
 vector<vector<char>> jugarGato(vector<vector<char>> matriz, int profundidad, int turnosRestantes){
     vector<vector<char>> mejorMovimiento;
     int menorEval = FILAS + COLUMNAS;
-
-    if(!sonMatricesIguales(matriz, moverArriba(matriz, GATO)) && minimax(moverArriba(matriz, GATO), profundidad, true, turnosRestantes)<menorEval){
-        mejorMovimiento = moverArriba(matriz, GATO);
-        menorEval = minimax(moverArriba(matriz, GATO), profundidad, true, turnosRestantes);
-    }
-    if(!sonMatricesIguales(matriz, moverDerecha(matriz, GATO)) && minimax(moverDerecha(matriz, GATO), profundidad, true, turnosRestantes)<menorEval){
-        mejorMovimiento = moverDerecha(matriz, GATO);
-        menorEval = minimax(moverDerecha(matriz, GATO), profundidad, true, turnosRestantes);
-    }
-    if(!sonMatricesIguales(matriz, moverIzquierda(matriz, GATO)) && minimax(moverIzquierda(matriz, GATO), profundidad, true, turnosRestantes)<menorEval){
-        mejorMovimiento = moverIzquierda(matriz, GATO);
-        menorEval = minimax(moverIzquierda(matriz, GATO), profundidad, true, turnosRestantes);
-    }
-    if(!sonMatricesIguales(matriz, moverAbajo(matriz, GATO)) && minimax(moverAbajo(matriz, GATO), profundidad, true, turnosRestantes)<menorEval){
-        mejorMovimiento = moverAbajo(matriz, GATO);
-        menorEval = minimax(moverAbajo(matriz, GATO), profundidad, true, turnosRestantes);
+    vector<char> direcciones ={'A', 'D', 'B', 'I'};
+    for(int d=0;d<4;d++){
+        vector<vector<char>> tableroAux;
+        if(comprobarMovimiento(matriz, GATO, direcciones[d]) && minimax(mover(matriz, GATO, direcciones[d]), profundidad, true, turnosRestantes) <= menorEval){
+            mejorMovimiento = mover(matriz, GATO, direcciones[d]);
+            menorEval = minimax(mover(matriz, GATO, direcciones[d]), profundidad, true, turnosRestantes);
+        }
     }
     return(mejorMovimiento);
-
 }
 
 vector<vector<char>> jugarRaton(vector<vector<char>> matriz, int profundidad, int turnosRestantes){
     vector<vector<char>> mejorMovimiento;
     int mayorEval = -1;
-
-    if(!sonMatricesIguales(matriz, moverArriba(matriz, RATON)) && minimax(moverArriba(matriz, RATON), profundidad, true, turnosRestantes)>mayorEval){
-        mejorMovimiento = moverArriba(matriz, RATON);
-        mayorEval = minimax(moverArriba(matriz, RATON), profundidad, true, turnosRestantes);
-    }
-    if(!sonMatricesIguales(matriz, moverDerecha(matriz, RATON)) && minimax(moverDerecha(matriz, RATON), profundidad, true, turnosRestantes)>mayorEval){
-        mejorMovimiento = moverDerecha(matriz, RATON);
-        mayorEval = minimax(moverDerecha(matriz, RATON), profundidad, true, turnosRestantes);
-    }
-    if(!sonMatricesIguales(matriz, moverIzquierda(matriz, RATON)) && minimax(moverIzquierda(matriz, RATON), profundidad, true, turnosRestantes)>mayorEval){
-        mejorMovimiento = moverIzquierda(matriz, RATON);
-        mayorEval = minimax(moverIzquierda(matriz, RATON), profundidad, true, turnosRestantes);
-    }
-    if(!sonMatricesIguales(matriz, moverAbajo(matriz, RATON)) && minimax(moverAbajo(matriz, RATON), profundidad, true, turnosRestantes)>mayorEval){
-        mejorMovimiento = moverAbajo(matriz, RATON);
-        mayorEval = minimax(moverAbajo(matriz, RATON), profundidad, true, turnosRestantes);
+    vector<char> direcciones ={'A', 'D', 'B', 'I'};
+    for(int d=0;d<4;d++){
+        vector<vector<char>> tableroAux;
+        if(comprobarMovimiento(matriz, RATON, direcciones[d]) && minimax(mover(matriz, RATON, direcciones[d]), profundidad, true, turnosRestantes) >= mayorEval){
+            mejorMovimiento = mover(matriz, RATON, direcciones[d]);
+            mayorEval = minimax(mover(matriz, RATON, direcciones[d]), profundidad, false, turnosRestantes);
+        }
     }
     return(mejorMovimiento);
-
 }
 
 
